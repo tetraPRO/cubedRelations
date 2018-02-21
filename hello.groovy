f86b111b08fc1e0b438f92f5352daab473df7bec
tetraPRO <caputo.philip@gmail.com>
2018-02-20 21:10:03 -0700

    defined a new class character

---
+++ hello.groovy
@@ -0,0 +1,18 @@
+/**
+* A class is the blueprint or definition
+* for the type of object that we need to
+* create in memory.
+*/
+class character{
+
+	def name
+	def age
+	def planet
+	def race
+	def profession
+	def hobby
+	def balance
+	def magic
+
+
+}
