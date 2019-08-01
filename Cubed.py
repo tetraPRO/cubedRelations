5727a0bcd67ba0bcfc1a3a2108e9d977c8aa70cf
Philip Caputo <caputo.philip@gmail.com>
2019-07-31 17:24:34 -0700

    Python Version: Cubed by summing

--- Cubed.py
+++ Cubed.py
@@ -1,3 +1,35 @@
-#!/usr/bin/python
+#!/usr/bin/python3
 
-def cube_by_summing:
+# This class defines a cube function by
+# summing 1 more than the multiples of
+#the number 6 which happens to equal x^3.
+
+def cubedBySumming(numOfTerms):#define method
+    result = 0#define variable
+
+    count = 1
+    while count < numOfTerms+1:
+        #herein lies the magic.  Why would one more than
+        #the multiples of 6 have anything to do with the cube of a number?
+
+        result += (1 + 6 * (sum(count)))#sum this computation
+
+        count += 1
+    return result
+
+
+def sum(n):#define method
+    result = 0#define variable
+
+    count = 0
+    while count < n:
+        result += count#sum this counter
+        count += 1
+
+    return result
+
+
+tester = input('Enter an integer: ')
+num = int(tester)
+assert cubedBySumming(num) == num**3
+print(f"The cube of {tester} is " + str(cubedBySumming(num)))
